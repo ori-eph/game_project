d3bf11d0beb78b6011d54896105a514835a90802
shoshana-solomyak <shoshanasolomyak@hilma.tech>
2023-10-15 15:34:38 +0300

    version b

---
+++ js/signUp.js
@@ -0,0 +1,32 @@
+
+let phoneNumber = document.getElementById("number").value
+
+let button = document.getElementById("submit")
+
+
+button.addEventListener("click", function () {
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    let phoneNumber = document.getElementById("number").value
+    let regex = /^\d{10}$|^\d{3}-\d{3}-\d{4}$|^\(\d{3}\)\d{3}-\d{4}$/
+    let test = regex.test(phoneNumber)
+    if (test === false) {
+        document.getElementById("invalid").innerHTML = "invalid number"
+    } else {
+        document.getElementById("invalid").innerHTML = ""
+        const newUser = {
+            username: username,
+            phoneNumber: phoneNumber,
+            password: password
+        }
+        localStorage.setItem("newUser", JSON.stringify(newUser))
+
+    }
+})
+console.log("Stored User: ", localStorage.getItem("newUser"));
+console.log("New User Object: ", newUser);
+
+
+
+
+
