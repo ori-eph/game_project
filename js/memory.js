a2abf3d43dbeee066708a69f4248e23627c42b09
shoshana-solomyak <shoshanasolomyak@hilma.tech>
2023-10-16 16:05:06 +0300

    memory bad

--- js/memory.js
+++ js/memory.js
@@ -9,14 +9,14 @@ for (let i = 0; i < 12; i++) {
 cardsNumbers = shuffleArray(cardsNumbers);
 
 //a function that fills the grid with cards;
-function createBoard(numCards) {   
-    cardsNumbersHalf = cardsNumbers.slice(0, numCards/2);
+function createBoard(numCards) {
+    cardsNumbersHalf = cardsNumbers.slice(0, numCards / 2);
     cardsNumbersSecondHalf = cardsNumbersHalf;
     cardsNumbers = cardsNumbersHalf.concat(cardsNumbersSecondHalf);
     cardsNumbers = shuffleArray(cardsNumbers);
 
     let grid = document.getElementById("grid-container");
-    grid.style.gridTemplateColumns = "repeat(" + numCards / (numCards/4) + ", 1fr)";
+    grid.style.gridTemplateColumns = "repeat(" + numCards / (numCards / 4) + ", 1fr)";
     grid.style.gridTemplateRows = "repeat(" + numCards / 4 + ", 1fr)";
 
     for (let i = 0; i < numCards; i++) {
@@ -26,7 +26,7 @@ function createBoard(numCards) {
         cardImg.style.objectFit = "cover";
         // card.appendChild(cardImg);
         cardImg.src = "../media/img/logo1.jpeg";
-        cards.push({ num: (i + 1), cardImg: "../media/img/cards/" + cardsNumbers[i] +".jpeg" })
+        cards.push({ num: (i + 1), cardImg: "../media/img/cards/" + cardsNumbers[i] + ".jpeg", cardNum: cardsNumbers[i] })
         grid.appendChild(cardImg);
     }
 }
@@ -44,3 +44,41 @@ function shuffleArray(array) {
     }
     return copy;
 }
+
+
+let open = 0
+let points = 0
+let firstCard = 0
+function playerTurn() {
+    let cards = document.getElementsByTagName("img");
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", function () {
+            if (open < 2) {
+                open++
+                cards[i].src = `../media/img/cards/${i}.jpeg`
+                if (open === 1) {
+                    firstCard = cards[i]
+                } else if (open === 2) {
+                    let secondCard = cards[i]
+                    if (secondCard.src === firstCard.src) {
+                        points++
+                    } else {
+                        setTimeout(function (first,second) {
+                            return function() {
+                                first.src = "../media/img/logo1.jpeg";
+                                second.src = "../media/img/logo1.jpeg";
+                                open = 0;
+                                firstCard = null;
+                            };
+                        }(firstCard, secondCard), 1000);
+                }
+            }
+
+        })
+    }
+}
+
+playerTurn()
+
+
+
