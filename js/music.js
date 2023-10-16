7f51b61fd6a78f405150f412537f7d69c3b8d05a
ori-eph <oriephrati@hilma.tech>
2023-10-16 15:59:28 +0300

    not working save recording

--- js/music.js
+++ js/music.js
@@ -30,19 +30,20 @@ createPiano(24);
 let keys = document.getElementById("keys").children;
 for (let i = 0; i < keys.length; i++) {
     keys[i].addEventListener("click", playKey);
-    keys[i].classList.add("cover");
 }
 
 function playKey() {
     let audio = this.children[0];
     audio.play();
-    if(isOn){
+    if (isOn) {
         recording.push(audio);
-     }
+    }
+    // colorKey(this);
 }
 
-let recording=[]
+let recording = []
 let isOn = false
+let recordingsNum = 0;
 
 let recordBtn = document.getElementById("record-button")
 let stopBtn = document.getElementById("stop-record")
@@ -51,10 +52,10 @@ let stopBtn = document.getElementById("stop-record")
 
 recordBtn.addEventListener("click", function () {
     isOn = true
-    recording=[]
+    recording = []
     recordBtn.classList.add("pressed")
     stopBtn.classList.remove("pressed")
-  
+
 })
 
 
@@ -62,7 +63,7 @@ let pianoNotes = document.getElementById("piano-notes");
 pianoNotes.addEventListener("keypress", playKeyKeyboard)
 
 function playKeyKeyboard(event) {
-   
+
     let keysString = "wertyuioasdfghjklzxcvbnm";
     let keys = keysString.split("");
     let capsKeys = keys.map(key => key.toUpperCase());
@@ -70,45 +71,63 @@ function playKeyKeyboard(event) {
         if (event.key === keys[i] || event.key === capsKeys[i]) {
             let audio = document.getElementsByTagName("audio");
             audio[i].play();
-        
-            if(isOn){
+
+            if (isOn) {
                 recording.push(audio[i]);
-             }
+            }
             pianoNotes.value = "";
-           
-            let key = document.getElementById("key" + i); 
-    key.classList.add("playing");
-} 
+
+            const key = document.getElementById("key" + (i + 1));
+            key.style.transition = "background-color 500ms ease-out";
+            key.style.backgroundColor = "#E1AA74";
+            setTimeout(() => {
+                key.style.backgroundColor = "white";
+            }, 350);
+            // colorKey(key);
         }
     }
-   
+}
+
 stopBtn.addEventListener("click", function () {
     isOn = false
     stopBtn.classList.add("pressed")
     recordBtn.classList.remove("pressed")
     playAudioTags(recording);
+    saveRecording(recording);
 })
 
+function saveRecording(recording) {
+    let recordingString = [];
+    for (let i = 0; i<recording.length; i++) {
+        recordingString[i] = recording[i].outerHTML;
+        console.log(recording[i]);
+    }
+    localStorage.setItem("recording" + (recordingsNum +1), JSON.stringify(recordingString));
+}
+
 function playAudioTags(thisRecording) {
+    console.log(recording)
     let i = 0;
     const playNext = () => {
-      if (i < thisRecording.length) {
-        thisRecording[i].play();
-        i++;
-        setTimeout(playNext, 400);
-      }
+        if (i < thisRecording.length) {
+            thisRecording[i].play();
+            i++;
+            setTimeout(playNext, 400);
+        }
     };
     playNext();
-  }
+}
 
-// function record() {
-//    if(isOn){
-//     let audio = document.getElementsByTagName("audio");
-//     for (let i = 0; i < audio.length; i++) {
-//         recording.push(audio[i]);
-//     }
-//    }
-   
-//     }
-    
 
+//not working, need to change saving way to numbers
+function convertRecording(recordingString) {
+    let converted = JSON.parse(recordingString);
+    return converted;
+}
+
+// function colorKey(key) {
+//     key.style.boxShadow = "inset 5px -5px 5px #727274a6";
+//     setTimeout(() => {
+//         key.style.boxShadow = "";
+//     }, 350);
+// }
