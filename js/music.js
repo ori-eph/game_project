2094724a3cddcf761abd6761555a2758c3f1f2fa
shoshana-solomyak <shoshanasolomyak@hilma.tech>
2023-10-17 17:59:26 +0300

    drums

--- js/music.js
+++ js/music.js
@@ -80,7 +80,7 @@ function playKey() {
     if (isOn) {
         for (let i = 0; i < audio.length; i++) {
             if (audio[i] === KeyAudio) {
-                recording.push(i);
+                recording.push({ type: "piano", index: Array.from(audio).indexOf(KeyAudio) });
             }
         }
     }
@@ -149,7 +149,13 @@ correspond to each number a second after the other (or more if the key was playe
 function playAudioTags(thisRecording) {
     let i = 0;
     const playNextHelper = () => {
-        audio[thisRecording[i]].play();
+        const sound=thisRecording[i];
+        if(sound.type==="piano"){
+          audio[sound.index].play();  
+        }else if(sound.type==="drum"){
+            drumAudio[sound.index].play();
+        }
+        
         i++;
         setTimeout(playNext, 500);
     }
@@ -256,3 +262,60 @@ function deleteItem() {
 window.onload = function () {
     refreshList("");
 };
+
+
+
+
+//drums
+
+const drumsContainer = document.getElementById("drums");
+
+// Function to create drum elements and their audio tags
+function createDrums() {
+    const numDrums = 6; // Number of drums
+    for (let i = 1; i <= numDrums; i++) {
+        // Create drum div
+        const drum = document.createElement("div");
+        drum.className = "drum"; // Add CSS class "drum"
+        drum.id = "drum" + i; // Assign unique id to each drum
+        drumsContainer.appendChild(drum); // Append drum to drums container
+
+        // Create drum audio
+        const audio = document.createElement("audio");
+        const source = document.createElement("source");
+        let sourceFile = `../media/sound/drums/drum${i}.mp3`; // Drum sound file path
+        source.src = sourceFile;
+        audio.appendChild(source);
+        drum.appendChild(audio); // Append audio to drum div
+
+        // Adjust audio settings
+        audio.playbackRate = 3;
+        audio.volume = 1.0;
+    }
+}
+
+createDrums(); // Call the function to create drums on load
+
+// Add event listeners for drum clicks
+const drumElements = document.getElementsByClassName("drum");
+for (let i = 0; i < drumElements.length; i++) {
+    drumElements[i].addEventListener("click",playDrum);
+}
+
+
+const drumAudio = document.querySelectorAll(".drum audio");
+
+
+function playDrum(){
+    const audio = this.getElementsByTagName("audio")[0]; 
+    audio.play();
+    if (isOn) {
+            for (let i = 0; i < drumAudio.length; i++) {
+                if (drumAudio[i] === audio) {
+                    recording.push({ type: "drum", index: Array.from(drumAudio).indexOf(audio) }); {
+              
+            }
+        }
+    }
+}}
+
